2eaf25cb36076ce28b775f7851006f0330d3acdd
NigeyNige <niallslater@gmail.com>
2018-06-21 14:32:54 +0100

    Added book formats

--- js/bookTitleGenerator.js
+++ js/bookTitleGenerator.js
@@ -9,84 +9,176 @@ var verbs = ["catch", "study", "follow", "love", "build", "kiss", "smaench", "ea
 
 var problems = ["Conundrum", "Case", "Affair", "Problem", "Referendum", "Decision", "Ultimatum", "Incident", "Resignation", "Discovery", "Rejection", "Cock-Up", "Disaster", "Campaign", "Murder", "Throwdown", "Betrayal"];
 
+var adjectives = ["Engorged", "Slippery", "Fast", "Wanted", "Dangerous", "Heartbreaking", "Subversive", "Racist", "Nice", "Aromatic", "Broken"];
+
+var nouns_proper = ["Smaenching", "Plague", "Suffering", "Pleasure", "Food", "Stuff", "Complaining", "Torture", "Fun", "Bookselling", "Piling up Rocks", "Wasting Time", "Faffing About"];
 
 function generateTitleShort(interestTag)
 {
     var result = "";
     
-    var numberOfFormats = 11;
+    var numberOfFormats = 4;
     var formatSelect = Math.floor(Math.random() * (numberOfFormats));
 	
     switch(interestTag) {
     case 'technology':
-		if (formatSelect > 5) {
-        	result = "Understanding " + getWord(nounsTechnology) + "s";
-		} else {
-			result = getWord(nounsTechnology) + "s for Dummies";
+		switch (formatSelect) {
+			case 0: 
+        		result = "Understanding " + getWord(nounsTechnology) + "s";
+				break;
+			case 1:
+				result = getWord(nounsTechnology) + "s for Dummies";
+				break;
+			case 2:
+				result = "Modding your " + getWord(nounsTechnology);
+				break;
+			case 3:
+				result = "Most " + getWord(adjectives) + " " + getWord(nounsTechnology) + " of " + getRandomRecentYear();
+				break;
 		}
         break;
     case 'politics':
-		if (formatSelect > 5) {
-        	result = getWord(problems) + " in the " + getWord(nounsPolitics);
-		} else {
-			result = "The " + getWord(nationalities) + " " + getWord(nounsPolitics);
+		switch (formatSelect) {
+			case 0: 
+        		result = getWord(problems) + " in the " + getWord(nounsPolitics);
+				break;
+			case 1:
+				result = "The " + getWord(nounsPolitics) + " of " + getRandomRecentYear;
+				break;
+			case 2:
+				result = "One " + getWord(adjectives) + " " + getWord(nounsPolitics);
+				break;
+			case 3:
+				result = "To " + getWord(verbs) + " a " + getWord(nounsPolitics);
+				break;
 		}
         break;
     case 'space':
-		if (formatSelect > 5) {
-        	result = "The Science of a " + getWord(nounsSpace);
-		} else {
-			result = "To " + getWord(verbs) + " a " + getWord(nounsSpace);
+		switch (formatSelect) {
+			case 0: 
+				result = "The Science of a " + getWord(nounsSpace);
+				break;
+			case 1:
+				result = "To " + getWord(verbs) + " a " + getWord(nounsSpace);
+				break;
+			case 2:
+				result = getWord(verbs) + " Me in the " + getWord(nounsSpace);
+				break;
+			case 3:
+				result = "Would You " + getWord(verbs) + " in Space?";
+				break;
 		}
         break;
     case 'nature':
-		if (formatSelect > 5) {
-        	result = "Great " + getWord(nounsNature) + " Photos";
-		} else {
-			result = "The " + getWord(nationalities) + " " + getWord(nounsNature);
+		switch (formatSelect) {
+			case 0: 
+				result = "Great " + getWord(nounsNature) + " Photos";
+				break;
+			case 1:
+				result = "The " + getWord(nationalities) + " " + getWord(nounsNature);
+				break;
+			case 2:
+				result = "The " + getWord(nounsNature) + " that Could " + getWord(verbs);
+				break;
+			case 3:
+				result = "Would You " + getWord(verbs) + " a " + getWord(nounsNature) + "?";
+				break;
 		}
         break;
     case 'business':
-		if (formatSelect < 4) {
-        	result = "Running a " + getWord(nounsBusiness);
-		} else if (formatSelect < 7) {
-			result = "Optimise your " + getWord(nounsBusiness);
-		} else {
-			result = "The Great " + getWord(nationalities) + " " + getWord(nounsBusiness);
+		switch (formatSelect) {
+			case 0: 
+				result = "Running a " + getWord(nounsBusiness);
+				break;
+			case 1:
+				result = "Optimise your " + getWord(nounsBusiness);
+				break;
+			case 2:
+				result = "The Great " + getWord(nationalities) + " " + getWord(nounsBusiness);
+				break;
+			case 3:
+				result = getWord(nounsBusiness) + "s were more " + getWord(adjectives) + " in " + getRandomRecentYear();
+				break;
 		}
         break;
     case 'mindfulness':
-		if (formatSelect > 5) {
-        	result = "Save your " + getWord(nounsBusiness) + " with " + getWord(nounsMindfulness);
-		} else {
-        	result = "Basics of " + getWord(nounsMindfulness);
+		switch (formatSelect) {
+			case 0: 
+				result = "Save your " + getWord(nounsBusiness) + " with " + getWord(nounsMindfulness);
+				break;
+			case 1:
+				result = "Basics of " + getWord(nounsMindfulness);
+				break;
+			case 2:
+				result = "Be More " + getWord(adjectives) + " with Crystals";
+				break;
+			case 3:
+				result = getWord(verbs) + " for " + getWord(nounsMindfulness);
+				break;
 		}
+        break;
     case 'history':
-		if (formatSelect > 5) {
-        	result = getWord(nounsHistory) + " for Dummies";
-		} else {
-        	result = "Remembering " + getWord(nounsHistory);
+		switch (formatSelect) {
+			case 0: 
+				result = getWord(nounsHistory) + " for Dummies";
+				break;
+			case 1:
+				result = "Remembering " + getWord(nounsHistory);
+				break;
+			case 2:
+				result = "How to " + getWord(verbs) + " in " + getRandomOldYear() + "?";
+				break;
+			case 3:
+				result = "A Comprehensive History of " + getWord(nouns_proper);
+				break;
 		}
         break;
     case 'music':
-		if (formatSelect > 5) {
-        	result = "A Life in " + getWord(adjectivesMusic);
-		} else {
-        	result = "Best of 70's " + getWord(adjectivesMusic);
+		switch (formatSelect) {
+			case 0: 
+				result = getWord(adjectivesMusic) + " for Dummies";
+				break;
+			case 1:
+				result = "A Life in " + getWord(adjectivesMusic);
+				break;
+			case 2:
+				result = "Best of 70's " + getWord(adjectivesMusic);
+				break;
+			case 3:
+				result = "Best of 80's " + getWord(adjectivesMusic);
+				break;
 		}
         break;
     case 'gaming':
-		if (formatSelect > 5) {
-        	result = "Secrets of " + getWord(nounsGaming);
-		} else {
-        	result = getWord(nounsGaming) + " Strategy Guide";
+		switch (formatSelect) {
+			case 0: 
+				result = "Secrets of " + getWord(nounsGaming);
+				break;
+			case 1:
+				result = getWord(nounsGaming) + " Strategy Guide";
+				break;
+			case 2:
+				result = "Scandals in " + getWord(nounsGaming);
+				break;
+			case 3:
+				result = "Tales from a " + getWord(nounsGaming) + " Expert";
+				break;
 		}
         break;
     case 'fantasy':
-		if (formatSelect > 5) {
-        	result = "The " + getWord(nounsFantasy) + "'s " + getWord(nounsGeneric);
-		} else {
-        	result = "The " + getWord(nounsPolitics) + " of " + getWord(nounsFantasy) + "s";
+		switch (formatSelect) {
+			case 0: 
+				result = "The " + getWord(nounsFantasy) + "'s " + getWord(nounsGeneric);
+				break;
+			case 1:
+				result = "The " + getWord(nounsPolitics) + " of " + getWord(nounsFantasy) + "s";
+				break;
+			case 2:
+				result = "To " + getWord(verbs) + " a " + getWord(nounsFantasy);
+				break;
+			case 3:
+				result = "Tales of a " + getWord(nounsFantasy);
+				break;
 		}
         break;
     default:
@@ -119,5 +211,15 @@ function generateName() {
 	
 	result = getWord(forenames) + " " + getWord(surnames);
 	
+	return result;
+}
+
+function getRandomRecentYear() {
+	let result = 2018 - 30 + Math.floor(Math.random() * 30);
+	return result;
+}
+
+function getRandomOldYear() {
+	let result = 1980 - 800 + Math.floor(Math.random() * 800);
 	return result;
 }
