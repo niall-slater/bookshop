0dffe064796920c05d06f638b668f53c16aaf514
Niall Slater <niallslater@gmail.com>
2018-06-27 19:28:00 +0100

    Added basic main menu

--- js/menu.js
+++ js/menu.js
@@ -26,9 +26,25 @@ var menuState = {
 		
 		
 		//set button functions
-		menuMain.panel.buttonNewGame.events.onInputUp.add(this.startGame);
+		menuMain.panel.buttonNewGame.events.onInputUp.add(this.openNewGameMenu);
 		menuMain.panel.buttonLoadGame.events.onInputUp.add(this.loadGame);
 		
+		
+		//set up new game panel
+		slickUI.add(menuStart.panel = new SlickUI.Element.Panel(0, 0, mapWidthDefault, mapHeightDefault));
+		
+		menuStart.panel.add(menuStart.panel.title = new SlickUI.Element.Text(110, 5, 'New Game', 14, styleDark));
+		menuStart.panel.add(menuStart.panel.buttonStart = new SlickUI.Element.Button(2, 32, 200, 32));
+		menuStart.panel.buttonStart.add(new SlickUI.Element.Text(8,4, 'Start', 10, styleDark));
+		menuStart.panel.add(menuStart.panel.buttonCancel = new SlickUI.Element.Button(2, 32 + 32 + 14, 200, 32));
+		menuStart.panel.buttonCancel.add(new SlickUI.Element.Text(8,4, 'Cancel', 10, styleDark));
+		
+		
+		//set button functions
+		menuStart.panel.buttonStart.events.onInputUp.add(this.startGame);
+		menuStart.panel.buttonCancel.events.onInputUp.add(this.cancel);
+		
+		menuStart.panel.visible = false;
 		
 		
 		//Set up fullscreen
@@ -37,11 +53,21 @@ var menuState = {
 		game.scale.refresh();
 	},
 	
-	startGame: function() {
-		game.state.start('play');
+	openNewGameMenu: function() {
+		menuMain.panel.visible = false;
+		menuStart.panel.visible = true;
 	},
 	
 	loadGame: function() {
 		console.log("Loading games isn't yet implemented. Soz!");
-	}
+	},
+	
+	cancel: function() {
+		menuStart.panel.visible = false;
+		menuMain.panel.visible = true;
+	},
+	
+	startGame: function() {
+		game.state.start('play');
+	},
 };
